68c2fa9c5ba69cb6f71a19dd8e8984ff3b9a60a6
Wendell Smith <wackywendell@gmail.com>
2018-12-02 02:16:44 -0500

    Started using clap

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,3 +1,16 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("My Super Program")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let input_path = matches.value_of("INPUT").unwrap();
+
+    println!("Using input {}", input_path);
 }
