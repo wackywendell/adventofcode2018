c53dc5849b54ea90b05aa4292ea02b62db7a5442
Wendell Smith <wackywendell@gmail.com>
2018-12-02 02:38:13 -0500

    Got day1 working

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,16 +1,34 @@
 use clap::{App, Arg};
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let matches = App::new("My Super Program")
         .arg(
-            Arg::with_name("INPUT")
-                .help("Sets the input file to use")
-                .required(true)
-                .index(1),
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("INPUT")
+                .takes_value(true),
         )
         .get_matches();
 
-    let input_path = matches.value_of("INPUT").unwrap();
+    let input_path = matches.value_of("INPUT").unwrap_or("inputs/day1.txt");
 
-    println!("Using input {}", input_path);
+    eprintln!("Using input {}", input_path);
+
+    let file = File::open(input_path)?;
+    let buf_reader = BufReader::new(file);
+
+    let mut sum = 0;
+    for (_i, line) in buf_reader.lines().enumerate() {
+        let s = line?;
+        let n = s.trim().parse::<i64>().unwrap();
+        sum += n;
+    }
+
+    println!("Final sum: {}", sum);
+
+    Ok(())
 }
