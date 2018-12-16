3a34075601381fa5ca99f78f3c5ee1a8704fb461
Wendell Smith <wackywendell@gmail.com>
2018-12-16 08:19:20 -0800

    Using a VecDeque makes much more sense than inserting in the middle

--- src/day9/main.rs
+++ src/day9/main.rs
@@ -1,26 +1,43 @@
 #![warn(clippy::all)]
 
+use std::collections::VecDeque;
+
 struct Game {
-    marbles: Vec<i64>,
-    current_ix: usize,
+    marbles: VecDeque<i64>,
     marble: i64,
     scores: Vec<i64>,
 }
 
 impl Game {
     fn new(players: usize) -> Game {
+        let mut ms = VecDeque::new();
+        ms.push_back(0);
         Game {
-            marbles: vec![0],
-            current_ix: 0,
+            marbles: ms,
             marble: 1,
             scores: vec![0; players],
         }
     }
 
+    fn rotate(&mut self, dist: isize) {
+        if self.marbles.len() < 2 {
+            return;
+        }
+        for _ in 0..dist {
+            let m = self.marbles.pop_back().unwrap();
+            self.marbles.push_front(m);
+        }
+
+        for _ in 0..-dist {
+            let m = self.marbles.pop_front().unwrap();
+            self.marbles.push_back(m);
+        }
+    }
+
     fn next(&mut self) {
         if self.marble % 23 == 0 {
-            self.current_ix = (self.current_ix + self.marbles.len() - 7) % self.marbles.len();
-            let removed = self.marbles.remove(self.current_ix);
+            self.rotate(-7);
+            let removed = self.marbles.pop_back().unwrap();
             let player = (self.marble as usize) % (self.scores.len());
 
             self.scores[player] += self.marble + removed;
@@ -28,8 +45,8 @@ impl Game {
             return;
         }
 
-        self.current_ix = (self.current_ix + 1) % self.marbles.len() + 1;
-        self.marbles.insert(self.current_ix, self.marble);
+        self.rotate(2);
+        self.marbles.push_back(self.marble);
         self.marble += 1;
     }
 
@@ -53,11 +70,8 @@ fn main() -> std::io::Result<()> {
     println!("High score: {:?}", g.high_score());
 
     let mut g = Game::new(459);
-    for i in 0..100 {
-        g.play(71320);
-        println!(".. {}", i);
-    }
-    println!("High score again: {:?}", g.high_score());
+    g.play(7132000);
+    println!("High score after 100x more rounds: {:?}", g.high_score());
 
     Ok(())
 }
@@ -70,13 +84,17 @@ mod tests {
     fn test_game() {
         let mut g = Game::new(9);
         g.play(25);
-        assert_eq!(
-            g.marbles,
-            vec!(
-                0, 16, 8, 17, 4, 18, 19, 2, 24, 20, 25, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7,
-                15
-            )
-        );
+
+        // Below is the order from the site.
+        // Actual order in the datastructure is reversed with the "active" marble
+        // at the end.
+        // assert_eq!(
+        //     g.marbles,
+        //     vec!(
+        //         0, 16, 8, 17, 4, 18, 19, 2, 24, 20, 25, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7,
+        //         15
+        //     )
+        // );
         assert_eq!(g.high_score(), 32);
     }
 
