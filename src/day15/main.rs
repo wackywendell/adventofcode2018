49c1e24962ab5a7d028de841dfedc033de27f080
Wendell Smith <wackywendell@gmail.com>
2018-12-24 17:39:09 -0800

    Passes most tests now

--- src/day15/main.rs
+++ src/day15/main.rs
@@ -36,6 +36,10 @@ impl Character {
     fn new(location: Location, hp: i64, side: Side) -> Self {
         Character { location, hp, side }
     }
+
+    fn attack_power(self) -> i64 {
+        3
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -89,7 +93,7 @@ impl Battle {
     fn empty_neighbors(&self, loc: Location, allow: Option<Location>) -> Vec<Location> {
         let (y, x) = loc;
         let mut locs: Vec<Location> = vec![(y - 1, x), (y, x - 1), (y, x + 1), (y + 1, x)];
-        // Keep neigbors that are (in allow) or (are viable squares and unoccupied)
+        // Keep neighbors that are (in allow) or (are viable squares and unoccupied)
         locs.retain(|&loc| {
             allow.map(|l| l == loc).unwrap_or(false)
                 || (self.squares.contains(&loc) && !self.occupied.contains(&loc))
@@ -100,20 +104,20 @@ impl Battle {
     // shortest_distance returns the (shortest distance, next step) from start to end,
     // if a path can be found.
     fn shortest_distance(&self, start: Location, end: Location) -> Option<(i16, Location)> {
-        #[derive(PartialEq, PartialOrd, Eq, Ord)]
+        #[derive(PartialEq, PartialOrd, Eq, Ord, Debug)]
         struct PartialPath {
-            dist: i16,
-            loc: Location,
-            first_step: Location,
-            // path: Vec<Location>,
             covered: i16,
+            dist: i16,
+            first_step: Location,
+            loc: Location,
+            path: Vec<Location>,
         };
 
         let mut partials: Vec<PartialPath> = vec![PartialPath {
             dist: start.dist(end),
             loc: start,
             first_step: start,
-            // path: Vec::new(),
+            path: Vec::new(),
             covered: 0,
         }];
 
@@ -121,7 +125,7 @@ impl Battle {
         seen.insert(start);
 
         loop {
-            partials.sort_by_key(|p| (-p.dist, p.first_step, p.loc));
+            partials.sort_by_key(|p| std::cmp::Reverse((p.covered, p.first_step, p.loc)));
             let popped = match partials.pop() {
                 None => {
                     // All paths ended in dead ends. No good.
@@ -149,36 +153,48 @@ impl Battle {
                 } else {
                     popped.first_step
                 };
-                // let mut path = popped.path.clone();
-                // path.push(n);
+                let mut path = popped.path.clone();
+                path.push(n);
                 let p = PartialPath {
                     dist: n.dist(end),
                     loc: n,
                     first_step,
                     covered: popped.covered + 1,
-                    // path: path,
+                    path: path,
                 };
                 partials.push(p);
             }
         }
     }
 
-    // Returns (next step, goal)
-    fn find_target(&self, character: Character) -> Option<(Location, Location)> {
+    // Returns (next step, goal, enemies_found)
+    fn find_target(&self, character: Character) -> Option<(Location, Location, bool)> {
         let mut choices = Vec::with_capacity((self.characters.len() - 1) * 4);
+        let mut enemies_found = 0;
         for target in &self.characters {
-            if target.side == character.side {
+            if target.side == character.side || target.hp <= 0 {
                 continue;
             }
+            enemies_found += 1;
 
             for empty in self.empty_neighbors(target.location, Some(character.location)) {
+                // println!("-- Checking empty at {:?}", empty);
                 let (dist, step) = match self.shortest_distance(character.location, empty) {
                     None => continue,
                     Some(sd) => sd,
                 };
 
+                // println!(
+                //     "Found target {:?}({}) at {:?} [{:?}]; distance {}, step {:?}",
+                //     target.side, target.hp, target.location, empty, dist, step
+                // );
+
                 choices.push((dist, empty, target.location, step));
             }
+        }
+
+        if enemies_found == 0 {
+            return Some((character.location, character.location, false));
         }
 
         choices.sort();
@@ -187,33 +203,113 @@ impl Battle {
         //     println!("{} - {:?} - {:?}", d, e, s);
         // }
 
-        choices.first().map(|&(_, _, t, s)| (s, t))
+        // if let Some((d, e, t, s)) = choices.first() {
+        //     println!("Choosing {} - {:?} - {:?} - {:?}", d, e, t, s);
+        // }
+
+        choices.first().map(|&(_, _, t, s)| (s, t, true))
     }
 
-    fn round(&mut self) {
+    fn target_to_attack(&mut self, c: Character) -> Option<&mut Character> {
+        let mut target = None;
+        for t in self.characters.iter_mut() {
+            if t.side == c.side || t.hp <= 0 || c.location.dist(t.location) != 1 {
+                continue;
+            }
+            match target {
+                None => {
+                    target = Some(t);
+                }
+                Some(ref m) if m.hp <= t.hp => {}
+                Some(_) => target = Some(t),
+            };
+        }
+
+        target
+    }
+
+    fn round(&mut self) -> bool {
         for ix in 0..self.characters.len() {
             let mut c = self.characters[ix];
             if c.hp <= 0 {
                 // Dead characters don't move
                 continue;
             }
-            let (step, mut target) = match self.find_target(c) {
+            let (step, _goal, any_enemies) = match self.find_target(c) {
                 None => {
+                    // println!("Can't move {:?} at {:?}", c.side, c.location);
                     // This character can't reach any enemies.
                     // No moving or attacking.
                     continue;
                 }
                 Some(st) => st,
             };
+            if !any_enemies {
+                return false;
+            }
             // Move. This may be a no-op if we're already next to a target.
-            c.location = step;
+            if c.location != step {
+                // println!(
+                //     "Moving {:?} at {:?} to {:?} (goal: {:?})",
+                //     c.side, c.location, step, goal
+                // );
+                self.occupied.remove(&c.location);
+                c.location = step;
+                self.occupied.insert(c.location);
+            }
             self.characters[ix] = c;
 
             // Attack.
-            if c.location.dist(target) == 1 {
-                target.hp -= c.attack_power();
+            let to_remove = if let Some(t) = self.target_to_attack(c) {
+                // println!(
+                //     "Attack by {:?}({}) at {:?} against {:?}({}) at {:?}",
+                //     c.side, c.hp, c.location, t.side, t.hp, t.location
+                // );
+                t.hp -= c.attack_power();
+                if t.hp <= 0 {
+                    Some(t.location)
+                } else {
+                    None
+                }
+            } else {
+                None
+            };
+
+            // Mark spots of dead characters as unoccupied.
+            if let Some(loc) = to_remove {
+                self.occupied.remove(&loc);
             }
         }
+
+        self.characters.sort();
+        return true;
+    }
+
+    fn get_characters(&self) -> Vec<Character> {
+        self.characters
+            .iter()
+            .filter_map(|&c| if c.hp > 0 { Some(c) } else { None })
+            .collect()
+    }
+
+    // Run to completion. Returns (# of rounds, total hp, side that won)
+    fn complete(&mut self) -> (usize, i64, Side) {
+        let mut n = 0;
+        while self.round() {
+            n += 1;
+        }
+
+        let mut side = Side::Elf;
+        let mut hp = 0;
+        for c in &self.characters {
+            if c.hp <= 0 {
+                continue;
+            }
+            side = c.side;
+            hp += c.hp;
+        }
+
+        (n, hp, side)
     }
 }
 
@@ -292,7 +388,7 @@ mod tests {
 
         let &c = battle.characters.first().unwrap();
         assert_eq!(c.location, (1, 1));
-        let (s, g) = battle.find_target(c).unwrap();
+        let (s, g, _) = battle.find_target(c).unwrap();
         println!("{:?}  {:?}", s, g);
         assert_eq!(s, (1, 2));
         assert_eq!(g, (1, 4));
@@ -302,20 +398,47 @@ mod tests {
     fn test_far_targeting() {
         let test_input = r#"
 #######
-#.E...#
+#.....#
+#..E..#
+#.....#
+#..####
+#.....#
 #..##.#
-####..#
-#G....#
+#####.#
+#...G.#
 #######"#;
 
         let battle = get_test_battle(test_input);
         assert_eq!(battle.characters.len(), 2);
         assert_eq!(battle.occupied.len(), battle.characters.len());
-        assert_eq!(battle.squares.len(), 15);
+
+        let &c = battle.characters.first().unwrap();
+        assert_eq!(c.location, (2, 3));
+        println!("Find target!");
+        let (s, g, _) = battle.find_target(c).unwrap();
+        println!("{:?}  {:?}", s, g);
+        assert_eq!(s, (2, 2));
+        assert_eq!(g, (8, 4));
+    }
+
+    #[test]
+    fn test_blocked_targeting() {
+        let test_input = r#"
+#######
+#.E...#
+#..##.#
+#E##..#
+#G....#
+#######"#;
+
+        let battle = get_test_battle(test_input);
+        assert_eq!(battle.characters.len(), 3);
+        assert_eq!(battle.occupied.len(), battle.characters.len());
+        assert_eq!(battle.squares.len(), 16);
 
         let &c = battle.characters.first().unwrap();
         assert_eq!(c.location, (1, 2));
-        let (s, g) = battle.find_target(c).unwrap();
+        let (s, g, _) = battle.find_target(c).unwrap();
         println!("{:?}  {:?}", s, g);
         assert_eq!(s, (1, 3));
         assert_eq!(g, (4, 1));
@@ -338,9 +461,177 @@ mod tests {
 
         let &c = battle.characters.first().unwrap();
         assert_eq!(c.location, (1, 2));
-        let (s, g) = battle.find_target(c).unwrap();
+        let (s, g, _) = battle.find_target(c).unwrap();
         println!("{:?}  {:?}", s, g);
         assert_eq!(s, (1, 2));
         assert_eq!(g, (1, 3));
+    }
+
+    fn get_test_battle_with_hps(s: &str, hps: &[i64]) -> Battle {
+        let mut battle = get_test_battle(s);
+        assert_eq!(battle.characters.len(), hps.len());
+        for (mut c, &hp) in battle.characters.iter_mut().zip(hps) {
+            c.hp = hp;
+        }
+
+        battle
+    }
+
+    #[test]
+    fn test_rounds() {
+        let initial = r"
+#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######";
+
+        let round1_str = r"
+#######
+#..G..#
+#...EG#
+#.#G#G#
+#...#E#
+#.....#
+#######";
+
+        let mut battle = get_test_battle(initial);
+        println!("Running round 1");
+        battle.round();
+        let chars = battle.get_characters();
+
+        let expected_hps = &[200i64, 197, 197, 200, 197, 197];
+        let round1 = get_test_battle_with_hps(round1_str, expected_hps);
+        let exp_chars = round1.get_characters();
+        assert_eq!(chars, exp_chars);
+
+        let round2_str = r"
+#######
+#...G.#
+#..GEG#
+#.#.#G#
+#...#E#
+#.....#
+#######";
+
+        println!("Running round 2");
+        battle.round();
+        let chars = battle.get_characters();
+        let expected_hps = &[200i64, 200, 188, 194, 194, 194];
+        let round2 = get_test_battle_with_hps(round2_str, expected_hps);
+        let exp_chars = round2.get_characters();
+        assert_eq!(chars, exp_chars);
+
+        let round23_str = r"
+#######
+#...G.#
+#..G.G#
+#.#.#G#
+#...#E#
+#.....#
+#######";
+        let expected_hps = &[200i64, 200, 131, 131, 131];
+        let round23 = get_test_battle_with_hps(round23_str, expected_hps);
+        let exp_chars = round23.get_characters();
+
+        for n in 2..23 {
+            println!("Running round {}", n + 1);
+            battle.round();
+        }
+
+        let chars = battle.get_characters();
+
+        assert_eq!(chars, exp_chars);
+
+        let round24_str = r"
+#######
+#..G..#
+#...G.#
+#.#G#G#
+#...#E#
+#.....#
+#######";
+        let expected_hps = &[200i64, 131, 200, 128, 128];
+        let round24 = get_test_battle_with_hps(round24_str, expected_hps);
+        let exp_chars = round24.get_characters();
+
+        for n in 23..24 {
+            println!("Running round {}", n + 1);
+            battle.round();
+            println!("Occupied: {:?}", battle.occupied);
+        }
+
+        let chars = battle.get_characters();
+
+        assert_eq!(chars, exp_chars);
+
+        let round25_str = r"
+#######
+#.G...#
+#..G..#
+#.#.#G#
+#..G#E#
+#.....#
+#######";
+        let expected_hps = &[200i64, 131, 125, 200, 125];
+        let round25 = get_test_battle_with_hps(round25_str, expected_hps);
+        let exp_chars = round25.get_characters();
+
+        for n in 24..25 {
+            println!("Running round {}", n + 1);
+            battle.round();
+        }
+
+        let chars = battle.get_characters();
+
+        assert_eq!(chars, exp_chars);
+
+        let round47_str = r"
+#######
+#G....#
+#.G...#
+#.#.#G#
+#...#.#
+#....G#
+#######";
+
+        let expected_hps = &[200i64, 131, 59, 200];
+        let round47 = get_test_battle_with_hps(round47_str, expected_hps);
+        let exp_chars = round47.get_characters();
+
+        for n in 25..47 {
+            println!("Running round {}", n + 1);
+            let finished = battle.round();
+            assert!(finished);
+        }
+
+        let chars = battle.get_characters();
+
+        assert_eq!(chars, exp_chars);
+
+        println!("Running round {}", 48);
+        let finished = battle.round();
+        assert!(!finished);
+    }
+
+    #[test]
+    fn test_completion() {
+        let initial = r"
+#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######";
+
+        let mut battle = get_test_battle(initial);
+        let (rounds, hp, side) = battle.complete();
+
+        assert_eq!(rounds, 47);
+        assert_eq!(hp, 590);
+        assert_eq!(side, Side::Goblin);
     }
 }
